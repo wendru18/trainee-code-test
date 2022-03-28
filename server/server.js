85ac4c91c938e8ce0b8807cf6f4e90c104f3963b
Melek Gülsever <melek.guelsever@nentgroup.com>
2022-03-28 16:30:21 +0200

    Loading json

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const movies = require('./movies.json');
 
 // Constants
 const PORT = 8000;
@@ -11,6 +12,22 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+//TODO Display a single item based on ID
+app.get('/movies/:id', (req, res) => {
+  const movie = req.params.id;
+  res.json(movie);
+
+  // try {
+  //   res.send(await getOne(req.params.id));
+  // } catch( error ) {
+  //   res.send(error);
+  // }
+})
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
