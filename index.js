90a44885c14d96245ef8dcce43b664f7feffc84d
wendru18 <wendru18@gmail.com>
2022-05-11 23:53:18 +0200

    refactoring

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ class MovieAPI {
 	constructor(movies) {
 		this.movies = movies;
         this.initMovies();
+        this.stringProperties = ["title", "description", "subtitle", "thumb", "genre"]
 	}
 
-    // When instantiating the class with the imported movies.json file, add an “id” and a random “rating” from 1 to 5 for each movie before storing it.
     initMovies(){
         for(var movie_id in this.movies){
             var movie = this.movies[movie_id]
@@ -13,46 +13,85 @@ class MovieAPI {
         }
     }
 
+    standardise(p, v){
+        if(this.stringProperties.includes(p))
+            return v.toLowerCase();
+        return v;
+    }
+
 	fetchAllMovies(){
 		return this.movies;
 	}
 
-    // A method that returns movies from a certain genre.
-    fetchMoviesFromGenre(wanted_genre){
-        return this.movies.filter(function(obj) {
-            var movie_genre = obj["genre"].toLowerCase();
-            wanted_genre = wanted_genre.toLowerCase();
-
-            return movie_genre == wanted_genre;
-        });
+    fetchMovies(property, value){
+        return this.movies.filter(x => this.standardise(property, x[property]) == this.standardise(property, value));
     }
 
-    // A method that removes a movie with a certain id (if found).
     removeMovie(id){
-        this.movies = this.movies.filter(function(obj) {
-            return obj["id"] !== id;
+        return this.movies.filter(x => { return x["id"] !== id });
+    }
+
+    filter(properties){
+        return this.movies.filter(x => {
+            for(var prop in properties)
+                delete x[properties[prop]];
+            return x;
+            }
+        );
+    }
+
+    // A method that returns the movies sorted by name.
+    //* property -> "title", "rating", ...
+    //* 0 -> ascending, 1 -> descending
+    sort(property, order){
+        return this.movies.sort((a, b) => {
+            var movie_a = this.standardise(property, a[property]);
+            var movie_b = this.standardise(property, b[property]);
+            if(order)
+                return (movie_a < movie_b) ? - 1 : (movie_a > movie_b) ? 1 : 0;
+            else
+                return (movie_a > movie_b) ? - 1 : (movie_a < movie_b) ? 1 : 0;
         });
     }
 
-    // A method that returns the movies with the subtitle and thumb properties filtered out.
-    filterSubtitleThumb(){
-        var properties = ["thumb", "subtitle"]
-        return this.movies.filter(function(obj) {
-            for(var prop in properties)
-                delete obj[properties[prop]];
-            return obj;
-        });
-    }
+
+
 }
 
 const moviesJson = require('./movies.json');
+
+// 1. (REQUIRED) When instantiating the class with the imported movies.json file, add an “id” and a random “rating” from 1 to 5 for each movie before storing it.
 const API = new MovieAPI(moviesJson);
-var allMovies = API.fetchAllMovies();
 
-const comedyMovies = API.fetchMoviesFromGenre("action");
+// 2. A method that returns movies from a certain genre.
 
-API.removeMovie(id = "0");
-allMovies = API.fetchAllMovies();
+// actionMovies = API.fetchMovies(property = "genre", value = "action");
+// console.log(actionMovies)
 
-console.log(API.filterSubtitleThumb())
+// 3. A method that removes a movie with a certain id (if found).
 
+// var allMovies = API.fetchAllMovies();
+// console.log(allMovies[0]["title"]);
+// allMovies = API.removeMovie(id = "0");
+// console.log(allMovies[0]["title"]);
+
+// 4. A method that returns the movies with the subtitle and thumb properties filtered out.
+
+// fitleredMovies = API.filter(["thumb", "subtitle"]);
+// console.log(fitleredMovies);
+
+// 5. A method that returns the movies sorted by name.
+
+// sortedMoviesByName = API.sort("name", 0);
+// console.log(sortedMoviesByName);
+
+//* 6. A method that returns the 2 top rated movies and 2 bottom rated movies.
+
+// sortedMovies = API.sort("rating", 0);
+// console.log(sortedMovies.slice(0, 2));
+// console.log(sortedMovies.slice(-2));
+
+//* 7. A method that prints out the three top rated movies.
+
+// sortedMovies = API.sort("rating", 0);
+// console.log(sortedMovies.slice(0, 3));
